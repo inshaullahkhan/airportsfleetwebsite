311c92c9b5dcb70264fa07cd0e010202bb7d568d
Insha Kundi <114442019+inshaullahkhan@users.noreply.github.com>
2023-08-25 11:54:20 +0500

    areas

---
+++ minicab-in-south-harrow.php
@@ -0,0 +1,319 @@
+<!DOCTYPE html>
+<html lang="en-GB"> 
+    <meta http-equiv="content-type" content="text/html;charset=UTF-8" />
+    <head>
+
+        <meta charset="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta content="width=device-width, initial-scale=1" name="viewport" />
+
+        <?php include('includes/head-variables.php') ?>
+
+        <title>Minicab in South Harrow - 020 3879 8080 - Taxi in South Harrow  </title>
+        <meta name="description" content="Call 020 3879 8080 - Minicabs in South Harrow, We provides 24 hours Minicab service from South Harrow, Heathrow, Gatwick, Stansted, Luton ..." />
+        <meta name="keywords" content="minicab in South Harrow to & from major airports including Gatwick, Heathrow, Luton & Stansted. Reliable airport cabs & mini cab service in South Harrow, London Taxi , London Minicabs , London Cabs,Taxi London ,Minicabs London,Cabs London,Taxi London Airport"/> 
+
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+        
+        <?php include('includes/headfiles.php') ?>
+    
+
+
+    </head>
+    <body class="page-template page-template-tpl-king_composer page-template-tpl-king_composer-php page page-id-14 kingcomposer kc-css-system w2dc-body w2dc-imitate-mode">
+        <div class="boxed_wrapper">
+
+            <?php include('includes/header.php') ?>
+
+            <?php include('includes/slider.php') ?>
+            
+            
+            <!-- <?php include('includes/features.php') ?> -->
+
+            
+            <!-- main contents -->
+            <div class="about-content3">
+
+
+<section class="container">
+    <!-- <h4 style ="color: #F25C26;   " >Mini Van Delivery</h4> <br> -->
+    <br>
+    
+    <div class="row">
+        
+        <div class="col-md-6 about-content1">
+            <div>
+                
+                
+                </div>
+                
+                <div class="vertically-middle" >
+        <div class ="area-content">
+
+            
+            <h1 style ="margin-bottom:2rem; font-size: 26px " >24 Hours Minicab Company in South Harrow <br>  <a title ="Call Now" href="tel:02038798080">Tel: 020 3879 8080</a> </h1> 
+            
+        </div>
+    
+
+            <div  class ="courier-list">
+
+                
+                <ul> 
+                    <li><i class="fa fa-arrow-right" aria-hidden="true"></i>  <strong> South Harrow  to Heathrow T 1,2,3.  &nbsp;  &#160;     £30   
+
+  </strong> </li>
+                    <br>
+                    <li> <i class="fa fa-arrow-right" aria-hidden="true"></i>  <strong>South Harrow to Gatwick &nbsp;  &#160; &nbsp;  &#160;&nbsp;  &#160;&nbsp;  &#160;&nbsp;  &#160;&nbsp;  &#160;  £83   </strong> </li>
+                    <br>
+                    <li><i class="fa fa-arrow-right" aria-hidden="true"></i> <strong>South Harrow to Luton   &#160; &#160; &nbsp;  &#160;&nbsp;  &#160;&nbsp;  &#160;&nbsp;  &#160;&nbsp;  &#160;   £42  </strong> </li>
+                    <br>
+                    <li><i class="fa fa-arrow-right" aria-hidden="true"></i> <strong>South Harrow to London City    &#160;&nbsp;  &#160;&nbsp;  &#160;&nbsp;  &#160;  £57  </strong> </li>
+                    <br>    
+                    <li><i class="fa fa-arrow-right" aria-hidden="true"></i>  <strong>South Harrow to Stansted   &#160; &nbsp;  &#160;&nbsp;  &#160;&nbsp;  &#160;&nbsp;  &#160;&nbsp;  &#160;   £72 </strong> </li>
+                    
+                </ul>
+
+
+                <li>
+
+                </li>
+
+                
+                
+            </div>
+            <br>
+            <p style ="margin-bottom:5rem;"> South Harrow Minicab Easiest online cab booking and Fastest minicab service in South Harrow </p>
+
+</div>
+     
+ </div>
+
+
+ 
+ <div class="col-md-6">
+     
+     <div class="circle-image">
+         
+         <div>
+         <img src="assets/images/minicabs-in-london.webp" alt="Minicabs Service" title ="Minicabs in London">
+
+     
+         </div>
+       
+     </div>
+     
+ </div>
+        </div>
+
+      
+
+</section>        
+
+</div>   
+
+        
+    
+  <!-- section2 end -->
+  
+  <!-- <?php include('includes/main-services.php'); ?> -->
+  
+
+  
+  
+  <div>
+
+      <!-- <?php include('includes/reviews.php') ?> -->
+    </div>
+  
+
+
+ <!-- section5 -->
+
+ <div class="about-content7 list-content">
+
+     <h2 style ="text-align:center;">Our Unique Services Consists Of</h2>    
+     
+     <section class="container">
+         
+         <br>
+         
+         
+         <div class=" footer-g ">
+             
+             <!-- <div class="col-md-6 about-content1"> -->
+                 
+                 
+                 <div class="vertically-middle1" >
+                     
+                     
+                     <div class="list-style" >
+<ul>
+    <li> <i class="fa fa-check" ></i> Book online within 1 minute</li>
+    <li> <i class="fa fa-check" ></i> Download our IPhone app </li>
+    <li> <i class="fa fa-check" ></i>  Executive Cars</li>
+     <li> <i class="fa fa-check" ></i> Saloon Cars, Estate Cars, 5,6,7, & 8 Seaters.</li>
+     <li> <i class="fa fa-check" ></i>   Mini Buses</li>
+     <li> <i class="fa fa-check" ></i> Online Booking Facility.</li>
+     <li> <i class="fa fa-check" ></i>  Parcel Delivery</li>
+     <li> <i class="fa fa-check" ></i>  Courier Service</li>
+     <li> <i class="fa fa-check" ></i> 24 Hour Service</li>
+     <li> <i class="fa fa-check" ></i>  Business Accounts Welcome</li>
+     <li> <i class="fa fa-check" ></i>  Fully computerized</li>
+     
+     <li> <i class="fa fa-check" ></i>  No Fare Increase After Midnight</li>
+
+     <li> <i class="fa fa-check" ></i> Contact us 24/7 365 via telephone, email or live chat</li>
+
+     <li> <i class="fa fa-check" ></i>  Airport Transfer Specialists</li>
+     <li> <i class="fa fa-check" ></i>  Instant quote</li>
+
+
+</ul>
+
+            </div>
+
+
+</div>
+     
+ <!-- </div> -->
+
+
+ 
+ <!-- <div class="col-md-6"> -->
+     
+ 
+         
+
+     <div class="vertically-middle1" >
+                    
+
+                    <div class="list-style" >
+        <ul>
+            <li> <i class="fa fa-check" ></i>   On time arrival.</li>
+            <li> <i class="fa fa-check" ></i>  Very competitive price </li>
+            <li> <i class="fa fa-check" ></i>  Text messaging facility.</li>
+             <li> <i class="fa fa-check" ></i>Book VIA App or online.</li>
+             <li> <i class="fa fa-check" ></i>   Smartly dresses drivers</li>
+             <li> <i class="fa fa-check" ></i>  Track you minicab.</li>
+             <li> <i class="fa fa-check" ></i>Pay by Debit or Credit Card</li>
+             <li> <i class="fa fa-check" ></i> Book a mini cab online in less than 2 minutes in/from South Harrow</li>
+             <li> <i class="fa fa-check" ></i>  Executive vehicles in/from South Harrow</li>
+             <li> <i class="fa fa-check" ></i> Minicab with a baby car seat in/from South Harrow</li> 
+
+             <li> <i class="fa fa-check" ></i> Airport Transfer Specialists in/from South Harrow</li>
+
+             <li> <i class="fa fa-check" ></i> Parcel Delivery in/from South Harrow</li>
+
+             <li> <i class="fa fa-check" ></i> Mini Buses in/from South Harrow</li>
+
+             <li> <i class="fa fa-check" ></i>Saloon Cars, Estate Cars, 5,6,7, & 8 Seaters in/from South Harrow</li>
+
+        </ul>
+        
+                    </div>
+        
+        
+        </div>
+     </div>
+     
+ <!-- </div> -->
+        <!-- </div> -->
+
+      
+
+</section>        
+
+</div>        
+
+
+
+
+ <!-- section5 -->
+
+          
+<!-- section 6 -->
+
+
+<div class="about-content3">
+
+
+<section class="container">
+    <!-- <h4 style ="color: #F25C26;   " >Mini Van Delivery</h4> <br> -->
+    <br>
+    
+    <div class="row">
+
+ 
+ <div class="col-md-12 about-content1">
+            <div>
+                
+                
+                </div>
+                
+                <div class="vertically-middle" >
+                    
+        
+                <h2>  <a title = "<?= $title; ?>" href="<?= $minicabsLondon ?>/minicabs-south-harrow">Minicabs in South Harrow</a> </h2>
+        
+
+            <div  class ="courier-list">
+
+            <p>
+        
+            We specialize in offering affordable, efficient, and competitive <a title ="Airport Transfer Service" href="airport-transfer"> airport transportation</a>. We provide coverage for all major airports, including London City, Gatwick, Stansted, and Heathrow. In all of London, South Harrow minicabs are renowned for their dependable courier service.
+            </p>
+
+            <p>
+            We take care of all your needs, taking you to well-known tourist attractions, eateries, hotels, theaters, clubs, and even to pick up takeout. You can hire a cab from South Harrow Minicabs for the entire day, and the journey can include a tour of famous sites in London.
+
+            </p>
+
+            </div>
+</div>
+     
+ </div>
+        </div>
+
+
+         <div class="para">
+ 
+         <h3>Book Now</h3>
+
+         <p>
+         Call Us Now <a title="Call Us Now" href = "tel:02038798080"> 020 3879 8080 </a>  24 Hours Minicab Company in South Harrow
+
+         </p>
+
+</div>
+
+
+</section>  
+
+</div>
+
+<!-- section 6 -->
+
+
+  <!-- section3 -->
+
+
+  <!-- <?php include('includes/airports.php'); ?> -->
+            
+        </div>
+
+
+        <!-- <?php include('includes/fleet.php'); ?> -->
+
+
+
+
+        <div class="clearfix"></div>
+
+
+
+            <?php include('includes/footerfiles.php'); ?>
+       
+      
+  </body>
